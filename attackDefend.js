5cf194007485ac2b38050e638ca1afb57f41ee00
ZackZielinski <Zackzielinski0@gmail.com>
2017-07-20 12:02:36 -0500

    started a damage calculating function

--- attackDefend.js
+++ attackDefend.js
@@ -1,12 +1,19 @@
 "use strict"
 
+var damageNumber;
+var blockNumber;
 
 function attackDamage(){
-	var damageNumber = Math.floor((Math.random() * 20) + 1);
+	damageNumber = Math.floor((Math.random() * 20) + 1);
 	document.getElementById('attack').innerHTML = "Dealt " + damageNumber + " Damage";
+	return damageNumber;
 }
 
 function damageBlocked(){
-	var blockNumber = Math.floor((Math.random() * 20) + 1);
+	blockNumber = Math.floor((Math.random() * 20) + 1);
 	document.getElementById('defend').innerHTML = "Blocked " + blockNumber + " Damage";
+	return blockNumber;
+}
+
+function damageDealt(){
 }
